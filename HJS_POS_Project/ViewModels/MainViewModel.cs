44726539de1f9e50070715ad6c69080f4656f461
GoldenCreature <jongsu3908@naver.com>
2026-06-26 14:33:16 +0900

    Add MainWindow UI and MainViewModel

--- HJS_POS_Project/ViewModels/MainViewModel.cs
+++ HJS_POS_Project/ViewModels/MainViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HJS_POS_Project.Views;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HJS_POS_Project.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        // 상품 관리 커맨드
+        public ICommand ProductCommand { get; set; }
+
+        // 판매 처리 커맨드
+        public ICommand SalesCommand { get; set; }
+
+        // 매출 통계 커맨드
+        public ICommand StatisticsCommand { get; set; }
+
+        // 생성자
+        public MainViewModel()
+        {
+            ProductCommand = new RelayCommand(OpenProduct);
+            SalesCommand = new RelayCommand(OpenSales);
+            StatisticsCommand = new RelayCommand(OpenStatistics);
+        }
+
+        // 상품 관리 화면 열기
+        private void OpenProduct()
+        {
+            ProductView productView = new ProductView();
+            productView.Show();
+        }
+
+        // 판매 처리 화면 열기
+        private void OpenSales()
+        {
+            SalesView salesView = new SalesView();
+            salesView.Show();
+        }
+
+        // 매출 통계 화면 열기
+        private void OpenStatistics()
+        {
+            StatisticsView statisticsView = new StatisticsView();
+            statisticsView.Show();
+        }
     }
 }
